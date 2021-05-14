72855063342edfbbd8a984d1932a4f702cc0adce
IzziGiuseppe <s1094052@studenti.univpm.it>
2021-05-14 18:44:10 +0200

    Inizio Statistiche

---
+++ statistica/model/Statistica.py
@@ -0,0 +1,6 @@
+class Statistica():
+    def __init__(self, nome, tipo, quantita):
+        super(Statistica, self).__init__()
+        self.nome = nome
+        self.tipo = tipo
+        self.quantita = quantita
