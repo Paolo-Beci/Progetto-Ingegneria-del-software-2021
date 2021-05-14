a1e4b6c43cd859207da18ca27bd470ea49a213a6
IzziGiuseppe <s1094052@studenti.univpm.it>
2021-05-14 19:51:34 +0200

    Definizione Statistica 1

--- statistica/model/Statistica.py
+++ statistica/model/Statistica.py
@@ -1,6 +1,7 @@
 class Statistica():
-    def __init__(self, nome, tipo, quantita):
+    def __init__(self, nome, tipo, quantita, contenuto):
         super(Statistica, self).__init__()
         self.nome = nome
         self.tipo = tipo
         self.quantita = quantita
+
